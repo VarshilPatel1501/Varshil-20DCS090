64c0f1813d2281f6bf5e31ef45f2d676a90e7976
VarshilPatel1501 <20dcs090@charusat.edu.in>
2022-09-07 18:04:52 +0530

    Updates

---
+++ precache-manifest.57437a606975ee92a49f7bf8e6a05719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c75dfa9a797971f2a239473f44d8097",
+    "url": "/Varshil-20DCS090/index.html"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/Varshil-20DCS090/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "a9a7effd1523af798c8c",
+    "url": "/Varshil-20DCS090/static/css/main.48551862.chunk.css"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/Varshil-20DCS090/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Varshil-20DCS090/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a7effd1523af798c8c",
+    "url": "/Varshil-20DCS090/static/js/main.1702761a.chunk.js"
+  },
+  {
+    "revision": "c545f1c3b3f59244dc48",
+    "url": "/Varshil-20DCS090/static/js/runtime-main.b162e457.js"
+  }
+]);
